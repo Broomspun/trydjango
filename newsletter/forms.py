e3b8ebd7c4c25d0d910e7459497a255ff11c17e3
Evan B <admin@storm-east.com>
2018-06-05 00:57:07 -0600

    Validation Form

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -7,3 +7,16 @@ class SignUpForm(forms.ModelForm):
         model = SignUp
         fields = ['full_name', 'email']
         #exclude = ['full_name']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split("@")
+        domain, extension = provider.split(".")
+        if not extension =="edu":
+            raise forms.ValidationError("Please use a valid .EDU email address")
+        return email
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data.get('full_name')
+        #Write validation code.
+        return full_name
