c0407fa434411d152c85aff21da4a461ca1489a4
Evan B <admin@storm-east.com>
2018-06-06 04:42:14 -0600

    Serving static files

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -132,3 +132,7 @@ STATICFILES_DIRS = [
 ]
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static_in_pro', 'static_root/img')
+
