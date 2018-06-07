9242a7542018af2e7ae767c8152f252c4e3e41cb
Evan B <admin@storm-east.com>
2018-06-07 10:48:26 -0600

    Update Django Login URL to Custom URL.

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -148,3 +148,4 @@ ACCOUNT_ACTIVATION_DAYS = 7  # One-week activation window; you may, of course, u
 REGISTRATION_AUTO_LOGIN = True  # Automatically log the user in.
 
 SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
