41204032f1624cbc3eca88c399e436eea105d516
Evan B <admin@storm-east.com>
2018-06-07 10:44:09 -0600

    Django Registration Redux - Customize Template

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -146,3 +146,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 ACCOUNT_ACTIVATION_DAYS = 7  # One-week activation window; you may, of course, use a different value.
 REGISTRATION_AUTO_LOGIN = True  # Automatically log the user in.
+
+SITE_ID = 1
